b28a1bc386e962231a0b6f8de682045d0b1e8c46
ShubhangiMule <Shubhangimule996@gmail.com>
2023-09-07 15:44:52 +0530

    variable assignment
    variable assignment

--- 01_variable.js
+++ 01_variable.js
@@ -9,3 +9,8 @@ console.log(city);
 var pin;
 console.log(pin);  //by default output is undefined
 
+var street;
+street="ravet";
+console.log(street);
+street="wakad"; //variable upation;
+console.log(street);
---
+++ 01_variableAssignAssignment.js
@@ -0,0 +1,9 @@
+var myFullName;
+console.log(myFullName);
+myFullName="shubhangi mule";
+console.log(myFullName);
+myFullName="shubhangi Dipak Mule";  // variable updation 
+console.log(myFullName);
+
+var myLove="I love Js";
+console.log(myLove);
