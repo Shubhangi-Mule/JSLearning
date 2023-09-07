33908fd370c4719f35b42aded4b0f1597e569c98
ShubhangiMule <Shubhangimule996@gmail.com>
2023-09-07 15:26:07 +0530

    variable declaration and initialization
    variable declaration and initialization

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+var myName; //variable declaration
+myName="shubhangi"; //variable assignment or initilization
+
+console.log(myName);
+
+var city="pune";    // variable declaration and initialization
+console.log(city);
+
+var pin;
+console.log(pin);  //by default output is undefined
+
