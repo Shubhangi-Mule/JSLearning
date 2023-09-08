13fab4037d63885550e8cabe521248c8d9bd98d8
ShubhangiMule <Shubhangimule996@gmail.com>
2023-09-08 16:03:55 +0530

    normal function
    normal function

---
+++ 03_normalFunAssignment.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName,accNumber,branch,pinCode){
+   console.log("bank details are --->",'bankName-',bankName,'accNumber-',accNumber,'branch-',branch,'pinCode-',pinCode);
+}
+bankDetails("CITIBank",652652762,"pune",276376);
+bankDetails("AxisBank",237376366,"Mumbai",345678);
+bankDetails("HDFC Bank",262537366,"Pune",345698);
